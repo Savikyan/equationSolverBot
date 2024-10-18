14e04d6a0f14de50a5d62972f278bc0ab6d2969c
Ivan Savitskyi <savikyan@gmail.com>
2024-10-18 18:18:58 +0300

    change commands

--- main.py
+++ main.py
@@ -7,18 +7,15 @@ API_TOKEN = '7846001329:AAGVTzk8JROCbVOcvNDQyt6EGAc3bqkAbf4'
 bot = telebot.TeleBot(API_TOKEN)
 
 # Handle '/start' and '/help'
-@bot.message_handler(commands=['help', 'start'])
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, """\
-Hi there, I am EchoBot.
-I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
-""")
+    bot.reply_to(message, "Привіт, я бот")
 
 
-# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
-@bot.message_handler(func=lambda message: True)
-def echo_message(message):
-    bot.reply_to(message, message.text)
+# # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+# @bot.message_handler(func=lambda message: True)
+# def echo_message(message):
+#     bot.reply_to(message, message.text)
 
 
 # Press the green button in the gutter to run the script.
